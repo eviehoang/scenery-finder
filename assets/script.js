adae4e9736a9c0cd4d2fe67ca924db49509d8f54
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-12 14:51:23 -0700

    added images from api to carousel

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,31 @@
-var unsplashUrl = "https://api.unsplash.com/photos/?";
-var unsplashKey = "client_id=c-IjSlno3hcniLXjGjNLHmA1cCJdEyXd2CuswiaMydo"
+var unsplashUrl = "https://api.unsplash.com/photos/random";
+var unsplashKey = "?client_id=c-IjSlno3hcniLXjGjNLHmA1cCJdEyXd2CuswiaMydo"
 
-var ipstackUrl = "http://api.ipstack.com/134.201.250.155?";
-var ipstackKey = "access_key=63132bface46b34e5f68472e399ebe92";
+var ipstackUrl = "http://api.ipstack.com/134.201.250.155";
+var ipstackKey = "?access_key=63132bface46b34e5f68472e399ebe92";
 
 var npsUrl = "https://developer.nps.gov/api/v1/parks?parkCode=acad";
 var npsKey = "&api_key=dAupn5zXkxUdXIomwX4fK9R6zhTDSV7j3bEW5IFp";
 
 
-
+const items = [
+    {
+        position: 0,
+        el: document.getElementById('carousel-item-1')
+    },
+    {
+        position: 1,
+        el: document.getElementById('carousel-item-2')
+    },
+    {
+        position: 2,
+        el: document.getElementById('carousel-item-3')
+    },
+    {
+        position: 3,
+        el: document.getElementById('carousel-item-4')
+    },
+];
 
 fetch(unsplashUrl + unsplashKey)
     .then(function (response) {
@@ -17,6 +34,7 @@ fetch(unsplashUrl + unsplashKey)
     })
     .then(function (data) {
         console.log('data', data)
+        items[0].el.children[0].setAttribute("src", data.urls.regular)
     })
 
 fetch(ipstackUrl + ipstackKey)
@@ -36,3 +54,4 @@ fetch(npsUrl + npsKey)
     .then(function (data) {
         console.log('data', data)
     })
+

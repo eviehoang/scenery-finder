53ac56ae05c8490c0a988326ac792168a6267326
Anthony Nguyen <anthonynguye56@gmail.com>
2023-10-13 10:55:45 -0700

    adding user input

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,10 @@ var ipstackKey = "?access_key=63132bface46b34e5f68472e399ebe92";
 var npsUrl = "https://developer.nps.gov/api/v1/parks?parkCode=acad";
 var npsKey = "&api_key=dAupn5zXkxUdXIomwX4fK9R6zhTDSV7j3bEW5IFp";
 
+var userInput = document.getElementById('default-search');
+var searchBtn = document.querySelector('#searchBtn');
 
-const items = [
+var items = [
     {
         position: 0,
         el: document.getElementById('carousel-item-1')
@@ -32,6 +34,8 @@ const items = [
 ];
 
 
+
+
 fetch(unsplashUrl + unsplashKey)
     .then(function (response) {
         console.log(response)
@@ -40,10 +44,14 @@ fetch(unsplashUrl + unsplashKey)
     .then(function (data) {
         console.log('data', data)
         for(var i=0; i <= 5; i++){
-            items[i].el.children[0].setAttribute("src", data.urls.regular)
+            items[i].el.children[0].setAttribute("src", data.urls.regular);
+            items[i].el.children[0].setAttribute("alt", data.alt_description);
         }
         
     })
+
+ipstackUrl = "http://api.ipstack.com/" + userInput.textContent.trim().toLowerCase();
+ipstackKey = "?access_key=63132bface46b34e5f68472e399ebe92";
 
 
 fetch(ipstackUrl + ipstackKey)
@@ -55,7 +63,7 @@ fetch(ipstackUrl + ipstackKey)
         console.log('data', data)
     })
 
-    
+
 fetch(npsUrl + npsKey)
     .then(function (response) {
         console.log(response)

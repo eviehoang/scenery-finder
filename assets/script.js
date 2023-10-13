a365da3a6ab3bb9249241462f55d8c02bdea8611
Evie Hoang <evie.h0325@gmail.com>
2023-10-13 14:36:28 -0700

    Removed unused variables, cleaned up id for html.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,10 @@
 var unsplashUrl = "https://api.unsplash.com/photos/random";
-
 var unsplashKey = "?client_id=c-IjSlno3hcniLXjGjNLHmA1cCJdEyXd2CuswiaMydo"
-
-// var ipstackUrl = "http://api.ipstack.com/134.201.250.155";
-// var ipstackKey = "?access_key=63132bface46b34e5f68472e399ebe92";
 
 var npsUrl = "https://developer.nps.gov/api/v1/parks?parkCode=acad";
 var npsKey = "&api_key=dAupn5zXkxUdXIomwX4fK9R6zhTDSV7j3bEW5IFp";
 
 var userInput = document.getElementById('default-search');
-// var searchBtn = document.querySelector('#searchBtn');
-var forwardBtn = document.getElementById('forward-button');
-var backBtn = document.getElementById('back-button');
 var unsplash = [];
 var index = 0;
 

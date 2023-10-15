8556235372c52942061ba964dbc81b3b27120fbc
Evie Hoang <evie.h0325@gmail.com>
2023-10-14 18:30:59 -0700

    Added variables for data pull for searched name, description and images into HTML.

--- assets/script.js
+++ assets/script.js
@@ -92,7 +92,7 @@ randomParks();
 // Parks by location
 function localParks(){
     var location = userInput.value;
-    var searchQuery = npsUrl + "limit=6&q=" + location + npsKey;
+    var searchQuery = npsUrl + "&q=" + location + npsKey;
 
     fetch(searchQuery)
         .then(function (response){
@@ -102,15 +102,34 @@ function localParks(){
             console.log('data', data);
             for(var i=0; i <=6; i++){
             var searchYield = document.getElementById("searchYield");
-            searchYield.append(card);
+            searchYield.innerHTML = card;
 
-                const cardHTML = `<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
-                        <div class="flex flex-col justify-between p-4 leading-normal">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            var description = data.data[i].description;
+            var name = data.data[i].name;
+            var img = data.data[i].images[2].url;
+
+                const cardHTML = `<article class="m-auto">
+                <div id="card">
+                    <div
+                        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <img class="rounded-t-lg" src="`+img+`" alt="" />
+                        <div class="p-5">
+                            <h4 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white p-2">
+                            `+name+`</h4>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">`+description+`</p>
+                            <a href="#"
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
                         </div>
-                    </></a>;`
+                    </div>
+                </div>
+            </article>`
 
             var card = cardHTML;
             
@@ -119,9 +138,6 @@ function localParks(){
         })
 
 }
-
-
-
 
 
 // Save last search history
